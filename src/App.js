3bb558f4eb348661a35584c2f0cdb6c1360c7c85
Triet Dang <mtriet193@gmail.com>
2023-01-12 10:43:27 +0700

    Exercise 3

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-// import Home from './Ex1/Home';
-
-import DataBinding from "./DemoDataBinding/DataBinding";
-import Loop from "./DemoDataBinding/Loop";
-import State from "./DemoDataBinding/State";
-import Car from "./Ex2_CarChangingColor/Car";
-
-import GrandPa from "./demoProps/GrandPa";
-
 
 import Home from "./ex3_Shopping/Home";
-import DemoRedux from "./demoRedux/DemoRedux";
 
-import DemoCart from "./demoRedux/DemoCart";
+
+
 function App() {
   return (
     <div className="App">
-    
-    {/* <Home></Home> */}
-
-    {/* <DataBinding></DataBinding>
-    
-    <State/> */}
-
-    {/* <Car/> */}
-
-    {/* <Loop/> */}
-    
-    {/* <GrandPa/> */}
-
-   {/* <DemoRedux/>  */}
-   <DemoCart></DemoCart>
-    {/* <Home></Home> */}
+    <Home></Home>
     </div>
   );
 }
